47b67abf438c624498c2b70b992ddddf8f81274d
joaobais <99919701+joaobais@users.noreply.github.com>
2022-10-10 20:23:23 -0300

    criando main.js e pegar inf API

---
+++ js/Grupos.js
@@ -0,0 +1,37 @@
+// selecoes vao vir pela API
+const timesContainer = document.getElementById("timesContainer");
+const grupoA = ["a", "b", "c", "a2", "b2", "c2"];
+const grupoB = [];
+const grupoC = [];
+const grupoD = [];
+const grupoE = [];
+const grupoF = [];
+const grupoG = [];
+const grupoH = [];
+const selecoesSorteadas = [];
+
+// Randomizando os times/selecoes
+
+/*function randomizeTeams() {
+  for (i = 0; i < grupoA.length; i++) {
+    const selecaoSorteada = Math.floor(Math.random() * grupoA.length);
+    const selecao = grupoA[selecaoSorteada];
+
+    while (selecoesSorteadas.includes(selecao)) {
+      //const selecaoSorteada = Math.floor(Math.random() * grupoA.length);
+      //const selecao = grupoA[selecaoSorteada];
+
+      //timesContainer.innerHTML += `${selecao} `;
+      console.log(`repetiu`);
+    }
+    timesContainer.innerHTML += `${selecao}-`;
+    selecoesSorteadas.push(selecaoSorteada);
+    console.log(selecoesSorteadas);
+  }
+}
+
+
+while (selecoesSorteadas.length < 7) {
+    for (i = 0; i < grupoA.length; i++)
+
+}*/
